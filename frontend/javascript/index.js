86abf81230ceb8eb9502ebebb486b1ca835d4aa5
Johnathan Lyman <jelyman@gmail.com>
2022-01-19 20:27:36 -0800

    initial commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,7 @@
+import "index.scss"
+
+// Import all JavaScript & CSS files from src/_components
+const componentsContext = require.context("bridgetownComponents", true, /\.(js|css)$/)
+componentsContext.keys().forEach(componentsContext)
+
+console.info("Bridgetown is loaded!")
