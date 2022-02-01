0520d03133d7468e2c5f0fa2249e1d1b5e26a7f0
Johnathan Lyman <jelyman@gmail.com>
2022-02-01 12:32:11 -0800

    wip

--- plugins/builders/raindrop.rb
+++ plugins/builders/raindrop.rb
@@ -1,5 +1,6 @@
 class Builders::Raindrop < SiteBuilder
   RAINDROP_API_ROOT = "https://api.raindrop.io/rest/v1".freeze
+  
   def build
     get "#{RAINDROP_API_ROOT}/collections", headers: {"Authorization" => "Bearer 6bd36be9-c55e-4cb9-ba0e-a4b9699fb8af"} do |data|
       p data
