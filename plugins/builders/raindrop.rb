c23fb96c61a50a06a300b1336c492843d3057d8a
Johnathan Lyman <jelyman@gmail.com>
2022-02-01 15:34:04 -0800

    Finish v1 of Raindrop collector

--- plugins/builders/raindrop.rb
+++ plugins/builders/raindrop.rb
@@ -1,9 +1,47 @@
 class Builders::Raindrop < SiteBuilder
-  RAINDROP_API_ROOT = "https://api.raindrop.io/rest/v1".freeze
-  
+  RAINDROP_API_ROOT = "https://api.raindrop.io/rest/v1"
+  HEADERS = {"Authorization" => "Bearer 6bd36be9-c55e-4cb9-ba0e-a4b9699fb8af"}
+
   def build
-    get "#{RAINDROP_API_ROOT}/collections", headers: {"Authorization" => "Bearer 6bd36be9-c55e-4cb9-ba0e-a4b9699fb8af"} do |data|
-      p data
+    hook :site, :post_read do
+      Bridgetown.logger.info "Starting Raindrip Collector"
+      link_structure = []
+
+      Bridgetown.logger.info "Querying Raindrop.io for collections"
+      collections = collect_collections
+
+      collections.each do |collection|
+        Bridgetown.logger.info "Querying Raindrop.io for collection ID #{collection._id}: #{collection.title}"
+        links = collect_links(collection._id)
+
+        link_output = []
+        links.each do |link|
+          link_output << {
+            title: link.title,
+            excerpt: link.excerpt,
+            url: link.link
+          }
+        end
+        
+        link_structure << {
+          category: collection.title,
+          id: collection._id,
+          links: link_output
+        }
+
+      end
+      site.data[:links] = link_structure
+      Bridgetown.logger.info "Raindrop Collector finished"
     end
+  end
+
+  def collect_collections
+    rd_collection_request = Faraday.get("#{RAINDROP_API_ROOT}/collections", {}, HEADERS)
+    JSON.parse(rd_collection_request.body, object_class: OpenStruct).items
+  end
+
+  def collect_links(collection_id)
+    request = Faraday.get("#{RAINDROP_API_ROOT}/raindrops/#{collection_id}", {}, HEADERS)
+    JSON.parse(request.body, object_class: OpenStruct).items
   end
 end
