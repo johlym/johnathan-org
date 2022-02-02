2210c5d952d8f290379dbd45749e7cd9bb7812f1
Johnathan Lyman <jelyman@gmail.com>
2022-02-01 17:21:27 -0800

    first swipe at tests

--- test/test_homepage.rb
+++ test/test_homepage.rb
@@ -10,5 +10,26 @@ class TestHomepage < Minitest::Test
     should "exist" do
       assert_select "body"
     end
+
+    should "contain the navbar" do
+      assert_select "nav"
+    end
+
+    should "contain the site title" do
+      assert_select "h2", /Johnathan Lyman/
+    end
+
+    should "contain ad script" do
+      assert_select "#ad-placement"
+    end
+
+    should "contain latest post header" do
+      assert_select "h2", /Latest Posts/
+    end
+
+    should "contain latest post content" do
+      assert_select "ul li div h3"
+    end
+
   end
 end
