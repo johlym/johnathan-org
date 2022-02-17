266ef2e17ea864f7c5618752f100513d3d370982
Johnathan Lyman <jelyman@gmail.com>
2022-02-16 20:03:48 -0800

    Instantiate the Bridgetown logger plugin-wide

--- plugins/site_builder.rb
+++ plugins/site_builder.rb
@@ -2,5 +2,9 @@ class SiteBuilder < Bridgetown::Builder
   require 'dotenv/load'
 
   # write builders which subclass SiteBuilder in plugins/builders
+
+  def log
+    Bridgetown.logger
+  end
 end
 
